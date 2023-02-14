95035c1b245a393b917d8230b2062d2e8d6abf8b
rosariohdz1012 <hernandezvillegasm12@gmail.com>
2023-02-13 23:28:06 -0600

    Actualizacion AppEscuela

--- src/AppEscuela.java
+++ src/AppEscuela.java
@@ -4,8 +4,8 @@ public class AppEscuela {
     public static void main(String[] args) {
 
 
-        Escuela.alumno("TE342","Maria del Rosario",22,
-                "rosario@gmail.com", "Ingenieria en Informatica");
+        Escuela.alumno("22TE0417M","Maria del Rosario",22,
+                "hernandezvillegasm12@gmail.com", "Ingenieria en Informatica");
 
         System.out.println("***********************************");
         Escuela.Materia("MA5PROG","POO",5,(byte)120,"Edgar Degante");
